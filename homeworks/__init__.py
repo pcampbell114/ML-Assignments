e8b5f799a5b15756953ea2d6780f3ed1ba3b5e1f
pcampbell14 <p.campbell@marquette.edu>
2026-09-12 01:27:31 -0700

    Keep only neural_network_mnist contents on this branch

--- homeworks/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import neural_network_mnist, kernel_bootstrap, intro_pytorch
-
-__all__ = ["neural_network_mnist", "kernel_bootstrap", "intro_pytorch"]
--- homeworks/intro_pytorch/losses/__init__.py
+++
@@ -1,4 +0,0 @@
-from .CrossEntropy import CrossEntropyLossLayer
-from .MSE import MSELossLayer
-
-__all__ = ["CrossEntropyLossLayer", "MSELossLayer"]
